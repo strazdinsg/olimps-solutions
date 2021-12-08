c194ee6fe6a508b49099a02f5176ab973442be79
Girts <girts.strazdins@gmail.com>
2021-12-08 21:03:20 +0100

    Proof of concept: Solved problem Nr 1: Reizināšana ar 2, 1 star

---
+++ 01-reiz/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+unsigned short readNumberFromFile(const char *filename);
+void writeNumberToFile(const char *filename, int number);
+
+int main() {
+    unsigned short n = readNumberFromFile("reiz.in");
+    writeNumberToFile("reiz.out", n * 2);
+    return 0;
+}
+
+void writeNumberToFile(const char *filename, int number) {
+    ofstream outputFile;
+    outputFile.open(filename);
+    outputFile << number;
+    outputFile.close();
+}
+
+unsigned short readNumberFromFile(const char *filename) {
+    ifstream inputFile;
+    inputFile.open(filename);
+    unsigned n = 0;
+    inputFile >> n;
+    inputFile.close();
+    return n;
+}

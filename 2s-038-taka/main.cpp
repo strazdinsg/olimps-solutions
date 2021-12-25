8f31fdc03cede5bdf490eaedc532b542ffb7aa48
Girts <girts.strazdins@gmail.com>
2021-12-25 23:49:35 +0100

    Solved problem nr 38 - Ceļojums
    The problem was much simpler than I initially thought.

--- 2s-038-taka/main.cpp
+++ 2s-038-taka/main.cpp
@@ -29,8 +29,6 @@ const unsigned char TURN_RIGHT = 'L';
 const unsigned char MOVE_FORWARD = 'S';
 const unsigned char END = 'V';
 
-const unsigned MAX_DISTANCE_IN_STEP = 1000;
-
 class Command {
     unsigned char type;
     int stepCount;
@@ -108,78 +106,12 @@ public:
         }
     }
 
-    unsigned getVerticalCommandCount(bool onlyVertical) const {
-        unsigned stepCount;
-        unsigned verticalDistance = abs(y);
-        // How many full 1000-step commands?
-        stepCount = verticalDistance / MAX_DISTANCE_IN_STEP;
-        if ((verticalDistance % MAX_DISTANCE_IN_STEP) > 0) {
-            // Some more steps left after the thousand
-            stepCount++;
-        }
-
-        if (onlyVertical) {
-            // Need to turn in the right direction
-            if ((y < 0 && heading == NORTH) || (y > 0 && heading == SOUTH)) {
-                // Must turn twice to get back in the right direction
-                stepCount += 2;
-            } else if (heading == WEST || heading == EAST) {
-                // When sideways, must turn once anyway
-                stepCount++;
-            }
-        }
-        return stepCount;
-    }
-
-    unsigned getHorizontalCommandCount(bool onlyHorizontal) const {
-        unsigned stepCount;
-        unsigned horizontalDistance = abs(x);
-        // How many full 1000-step commands?
-        stepCount = horizontalDistance / MAX_DISTANCE_IN_STEP;
-        if ((horizontalDistance % MAX_DISTANCE_IN_STEP) > 0) {
-            // Some more steps left after the thousand
-            stepCount++;
-        }
-
-        if (onlyHorizontal) {
-            // Need to turn in the right direction
-            if ((x < 0 && heading == WEST) || (x > 0 && heading == EAST)) {
-                // Moved North, must turn twice to go south now
-                stepCount += 2;
-            } else if (heading == NORTH || heading == SOUTH) {
-                // When facing north or south, must turn once anyway
-                stepCount++;
-            }
-        }
-        return stepCount;
-    }
-
-    bool headingAlongPerimeter() const {
-        return x > 0 && heading == WEST
-               || x < 0 && heading == EAST
-               || y > 0 && heading == NORTH
-               || y < 0 && heading == SOUTH;
-    }
-
-/**
-     * How many times the gnome must turn to start traveling back along the perimeter
+    /**
+     * The shortest distance is to go x to lef/right and y steps up/down
      * @return
      */
-    unsigned getTurnCommandCount() const {
-        return headingAlongPerimeter() ? 1 : 2;
-    }
-
-    unsigned getMinCommandCount() const {
-        if (x == 0 && y == 0) {
-            return 0;
-        } else if (x == 0) {
-            return getVerticalCommandCount(true);
-        } else if (y == 0) {
-            return getHorizontalCommandCount(true);
-        } else {
-            return getHorizontalCommandCount(false) + getVerticalCommandCount(false)
-                   + getTurnCommandCount();
-        }
+    unsigned getReturnStepCount() const {
+        return abs(x) + abs(y);
     }
 };
 
@@ -191,7 +123,7 @@ int main() {
             command = readNextCommandFromFile();
             gnome.act(command);
         }
-        writeAnswerToFile("taka.out", gnome.getMinCommandCount());
+        writeAnswerToFile("taka.out", gnome.getReturnStepCount());
     } else {
         return -1;
     }
